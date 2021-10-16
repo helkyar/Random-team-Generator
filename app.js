07e758b0665d0982f35c11447ec7c9064aa95c1b
helkyar <javierpalaciosbotejara@gmail.com>
2021-10-16 19:23:30 +0200

    Add remove member

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
-// elimar personas
-// ?-> del equipo
-// -> de la lista inicial
-
 // Local storage
-// Limpiar todo
-// Establecer mínima diferencia entre equipos (1 partc, 2, etc.) [Default 1]
-// Arrastar para cambiar de equipo
-// Indicar Número de participantes
-// Indicar número de equipos
+// Establecer máxima diferencia entre equipos (1 partc, 2, etc.) [Default 1]
+// Arrastar para cambiar de equipo (move whith css, if(ok){change innerText}, finish animation returning to original pos)
 
 const add = document.querySelector('.add');
 const create = document.querySelector('.create');
@@ -17,22 +10,21 @@ let size = document.querySelector('#size');
 let players = document.querySelector('.participants');
 let teams = document.querySelector('.team');
 
-let playerList = [];
-
 add.addEventListener('click', (e) => addToList(e));
 create.addEventListener('click', createTeam);
 
 function addToList(e) {
   e.preventDefault();
   if (input.value) {
-    players.innerHTML += `<p class="partc-name">${input.value}</p>`;
-    playerList.push(input.value);
+    players.innerHTML += `<p class="partc-name"><button>
+    <img src="img/trash.png" alt="" /></button>${input.value} </p>`;
     input.value = '';
     infoUpdate();
   }
 }
 
 function createTeam() {
+  let playerList = document.querySelectorAll('.partc-name');
   teams.innerHTML = '';
   let random = [];
   let number = 0;
@@ -49,7 +41,7 @@ function createTeam() {
     let team = `<strong>Team${j + a - finalsize * a++}</strong>`;
     for (let i = 0; i < finalsize; i++) {
       if (j == playerList.length) break;
-      team += `<p>${playerList[random[j++]]}</p>`;
+      team += `<p>${playerList[random[j++]].innerText}</p>`;
     }
     teams.innerHTML += `<div class="team-div">${team}</div>`;
   }
@@ -61,16 +53,25 @@ let infMemb = document.querySelector('.inf-memb');
 let infTeam = document.querySelector('.inf-teams');
 
 clear.addEventListener('click', empty);
-// size.addEventListener('change', infoUpdate);
 size.addEventListener('input', infoUpdate);
+players.addEventListener('click', (e) => remove(e));
 
 function empty() {
   playerList = [];
   players.innerHTML = '';
+  teams.innerHTML = '';
 }
 
 function infoUpdate() {
   let realSize = size.value || 2;
   infMemb.innerText = `Memb.: ${playerList.length}`;
   infTeam.innerText = `Teams: ${Math.ceil(playerList.length / realSize)}`;
+}
+
+function remove(e) {
+  if (e.target.tagName == 'BUTTON') {
+    e.target.parentElement.remove();
+  } else if (e.target.tagName == 'IMG') {
+    e.target.parentElement.parentElement.remove();
+  }
 }
