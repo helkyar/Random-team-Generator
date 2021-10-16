abac6d36f612beeb8d50210a8dba1ddd69ceabd4
helkyar <javierpalaciosbotejara@gmail.com>
2021-10-16 13:56:38 +0200

    Initial files

---
+++ app.js
@@ -0,0 +1,7 @@
+// Formulario que acepta nombres
+// Genera equipos de forma aleatoria
+// Número de personas por equipos
+// elimar personas
+// -> del equipo
+// -> de la lista inicial
+// ?->Arrastar para cambiar de equipo
