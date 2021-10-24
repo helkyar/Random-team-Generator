b59ea5f7f5891a7edd50411dd54c2bab2d5fa0e6
josemsb92 <josemsb92@gmail.com>
2021-10-24 21:56:02 +0200

    Add eventListener to "Add" Button

--- Team Generator Project/app.js
+++ Team Generator Project/app.js
@@ -7,10 +7,34 @@
 // ?->Arrastar para cambiar de equipos
 
 //Delete text from inputs
+var counter = 0;
 
-let participants = document.getElementsByClassName("participants");
-let element = document.createElement("p");
-element.innerText = "Josemi";
-console.log(element);
-console.log(participants);
-participants[0].appendChild(element);
+let addButton = document.querySelector(".add");
+
+let nameValue = document.querySelector("#name");
+
+let members = document.querySelector(".inf-memb");
+
+
+
+
+
+//Add event to the "Add" button, when it gets pressed, it wont refresh the page, 
+//but instead will add 1 to the counter variable.
+addButton.addEventListener('click',function(e){
+e.preventDefault();
+if(nameValue.value != ""){
+    counter+=1;
+}
+//  ESTO HAY QUE AVERIGUAR COMO: 
+//let texto = members.textContent
+//  texto[8] = "counter";
+// members.innerHTML = texto
+nameValue.value = "";
+
+
+console.log(counter)
+});
+
+
+
