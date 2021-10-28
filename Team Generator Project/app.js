886a29ff4f50652be333cfa634ed3f684db1ec22
josemsb92 <josemsb92@gmail.com>
2021-10-28 10:59:28 +0200

    Team Size event done

--- Team Generator Project/app.js
+++ Team Generator Project/app.js
@@ -11,7 +11,8 @@ let counter = 0;
 
 let addButton = document.querySelector(".add");
 
-let teamSizer = document.querySelector("#size");
+let teamSize
+ = document.querySelector("#size");
 
 let nameValue = document.querySelector("#name");
 
@@ -23,13 +24,23 @@ let team = document.querySelector(".team");
 
 let memberInit = memberList.innerHTML;
 
+let membSize;
+
 const teamInitial = team.innerHTML;
+
+//Create a "p" tag with the content of the "Name" input text and add it to the list of members.
+function createMember() {  
+  memberInit += `<p class="member">${nameValue.value}</p>`;
+  memberList.innerHTML = memberInit;
+  membSize = document.querySelectorAll(".member");
+  members.innerText = `Memb.: ${membSize.length}`;
+}
+
 //Add event to the "Add" button, when it gets pressed, it wont refresh the page,
 //but instead will add 1 to the counter variable.
 addButton.addEventListener("click", function (e) {
   //This function will prevent running any default event like reset or refresh the page
   e.preventDefault();
-
   if (nameValue.value != "") {    
     //Insert the value of "Name" text input into a new created paragraph tag, wich is a child
     //of section wich class is "participants"
@@ -37,24 +48,19 @@ addButton.addEventListener("click", function (e) {
   }
 });
 
-
-//Create a "p" tag with the content of the "Name" input text and add it to the list of members.
-
-function createMember() {  
-  memberInit += `<p class="member">${nameValue.value}</p>`;
-  memberList.innerHTML = memberInit;
-  let membSize = document.querySelectorAll(".member");
-  members.innerText = `Memb.: ${membSize.length}`;
-}
-
-
-
-//ON CONSTRUCTION
-teamSizer.addEventListener("input", function(){  
-  
+//ON CONSTRUCTION Cuando redimensiono el equipo hacia menos de 1 se queda pillado
+teamSize
+.addEventListener("input", function(){    
   team.innerHTML = teamInitial;
-  for(let i=0; i < teamSizer.value; i++){                  
-      team.appendChild(document.createElement("div"));        
+  teamSize
+  .min = 1;
+  if(teamSize
+    .value < 1){
+    teamSize
+    .value = 1;
+  }  
+  for(let i=0; i < membSize.length / teamSize.value; i++){                  
+      team.innerHTML +=`<div>Team ${i+1}</div>`;        
   };    
 
 })
