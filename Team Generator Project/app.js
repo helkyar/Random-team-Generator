5712df5dbd90d18738a38c694c847c6e065c1199
josemsb92 <josemsb92@gmail.com>
2021-10-28 21:56:30 +0200

    updated addButton event and teamSize event

--- Team Generator Project/app.js
+++ Team Generator Project/app.js
@@ -11,10 +11,9 @@ let counter = 0;
 
 let addButton = document.querySelector(".add");
 
-let teamSize
- = document.querySelector("#size");
+let teamSize = document.querySelector("#size");
 
-let nameValue = document.querySelector("#name");
+let nameInput = document.querySelector("#name");
 
 let members = document.querySelector(".inf-memb");
 
@@ -22,52 +21,48 @@ let memberList = document.querySelector(".participants");
 
 let team = document.querySelector(".team");
 
-let memberInit = memberList.innerHTML;
-
-let membSize="";
-
 let infTeams = document.querySelector(".inf-teams");
+ 
+let memberInit = [memberList.innerHTML];  
 
-const infTeamsInitial = infTeams.innerHTML;
-const teamInitial = team.innerHTML;
+let teamInitial = team.innerHTML;
 
 //Create a "p" tag with the content of the "Name" input text and add it to the list of members.
-function createMember() {  
-  memberInit += `<p class="member">${nameValue.value}</p>`;
-  memberList.innerHTML = memberInit;
-  membSize = document.querySelectorAll(".member");
-  members.innerText = `Memb.: ${membSize.length}`;
-}
+
 
 //Add event to the "Add" button, when it gets pressed, it wont refresh the page,
 //but instead will add 1 to the counter variable.
 addButton.addEventListener("click", function (e) {
   //This function will prevent running any default event like reset or refresh the page
   e.preventDefault();
-  if (nameValue.value != "") {    
+  function createMember() {    
+    memberInit.push(`<p class="member">${nameInput.value}</p>`);
+    nameInput.value = "";
+    memberList.innerHTML = memberInit.join("");   
+    members.innerText = `Memb.: ${memberInit.length-1}`;
+  }
+  if (nameInput.value != "") {    
     //Insert the value of "Name" text input into a new created paragraph tag, wich is a child
     //of section wich class is "participants"
     createMember();
-  }
-  
+  }  
 });
 
 //ON CONSTRUCTION Si añado el Team Size primero y luego los nombres, no me genera equipos hasta pinchar en Team Size.
-teamSize.addEventListener("input", function(){    
-  team.innerHTML = teamInitial;
-  teamSize.min = 1;
-  if(teamSize.value < 1){
-    teamSize.value = "";
-  }  
-  if(teamSize.value == ""){
-    infTeams.innerHTML = infTeamsInitial;
-  }
-  infTeams.innerHTML = `Teams: ${teamSize.value}`;
+teamSize.addEventListener("input", function(){   
+  let size = 2;
+  let teamDivs = [];  
 
-  if(membSize.value != ""){
-    for(let i=0; i < membSize.length / teamSize.value; i++){                  
-        team.innerHTML +=`<div>Team ${i+1}</div>`;    
-    }    
-  };     
+  if(teamSize.value > 2){
+    size = teamSize.value;
+  }    
+  let teamNum = Math.ceil((memberInit.length-1) / size);  
+  infTeams.innerHTML = `Teams: ${teamNum}`;
+  
+    for(let i=0; i < teamNum; i++){                  
+        teamDivs.push(`<div><h4>Team ${i+1}</h4></div>`);
+      };   
+              
+  team.innerHTML = teamInitial + teamDivs.join("");  
 
 })
