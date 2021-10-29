696a3d7049ce5e30096d99a9055f1905a5e40b34
helkyar <javierpalaciosbotejara@gmail.com>
2021-10-29 17:52:41 +0200

    Merge both branches

--- Team Generator Project/app.js
+++ Team Generator Project/app.js
@@ -1,3 +1,81 @@
+/**
+ * KNOWN ISSUES
+ * -addmember animation aplias to all all the time
+ */
+
+let addButton = document.querySelector('.add');
+let teamSize = document.querySelector('#size');
+let nameInput = document.querySelector('#name');
+
+let members = document.querySelector('.inf-memb');
+let infTeams = document.querySelector('.inf-teams');
+
+let memberList = document.querySelector('.list');
+
+let team = document.querySelector('.teams-wraper');
+
+let memberInit = [memberList.innerHTML]; //está vacío al principio
+let teamInitial = team.innerHTML; //está vacío al principio
+
+addButton.addEventListener('click', addMember);
+memberList.addEventListener('click', removeMemb);
+teamSize.addEventListener('input', addTeam);
+
+// ======================================================
+function addMember(e) {
+  e.preventDefault();
+
+  if (nameInput.value != '') {
+    memberInit.push(`<p class="member">
+      <img src="img/trash.svg" alt="delete" />${nameInput.value}</p>`);
+
+    nameInput.value = '';
+    memberList.innerHTML = memberInit.join('');
+
+    addTeam(); // Actualizar al meter los miembros UE
+  }
+}
+
+function removeMemb(e) {
+  let paragraf = e.target.parentElement;
+  if (e.target.tagName == 'IMG') {
+    paragraf.remove();
+  }
+  memberInit = memberInit.filter((val) => {
+    console.log(val, paragraf.innerText);
+    console.log(val.includes(paragraf.innerText));
+    return !val.includes(paragraf.innerText);
+  });
+  // remove from array, select player
+  infoUpdate(10);
+}
+
+function addTeam() {
+  let size = 2;
+  let teamDivs = [];
+
+  teamSize.value > 2 ? (size = teamSize.value) : '';
+  let teamNum = Math.ceil((memberInit.length - 1) / size);
+
+  infoUpdate(teamNum);
+  // Meter miembros => ???
+  // <p class='team-member'> Juan </p>;
+  for (let i = 0; i < teamNum; i++) {
+    teamDivs.push(`<article class="team"> <h4>Team ${i + 1}</h4>
+    <div class="team-member-list"><p><strong>???</strong></p></div></article><div></div>`);
+  }
+
+  team.innerHTML = teamInitial + teamDivs.join('');
+}
+// HELPERS --------------------------------------------
+
+function infoUpdate(teamNum) {
+  members.innerText = `Memb.: ${memberInit.length - 1}`;
+  infTeams.innerHTML = `Teams: ${teamNum}`;
+}
+
+// ======================================================
+
 let teamMember = document.querySelectorAll('.team-member');
 let body = document.querySelector('body');
 let drag = false;
