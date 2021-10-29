7fee3545b0a61e8f8cc85e61ce4e695d3087c9ea
helkyar <javierpalaciosbotejara@gmail.com>
2021-10-29 16:54:51 +0200

    add drag & drop

--- Team Generator Project/app.js
+++ Team Generator Project/app.js
@@ -1,12 +1,54 @@
-// Formulario que acepta nombres
-// Genera equipos de forma aleatoria
-// Número de personas por equipos
-// elimar personas
-// -> del equipo
-// -> de la lista inicial
-// ?->Arrastar para cambiar de equipos
+let teamMember = document.querySelectorAll('.team-member');
+let body = document.querySelector('body');
+let drag = false;
+let selected;
+let selY;
+let selX;
 
-//Delete text from inputs
-function clearText(id){
-   document.getElementById(id).value="";
+for (let i = 0; i < teamMember.length; i++) {
+  teamMember[i].addEventListener('mousedown', move);
+  teamMember[i].addEventListener('mouseup', remove);
+}
+
+document.addEventListener('mouseup', removeDoc);
+document.onmusedown = () => {
+  return false;
+};
+function move(e) {
+  if (e.target.classList.contains('team-member')) {
+    drag = true;
+    selected = e.target;
+    selY = e.clientY - 30;
+    selX = e.clientX;
+    body.classList.add('unselectable');
+    document.addEventListener('mousemove', mousemove, true);
+  }
+}
+
+function removeSelection() {
+  return false;
+}
+function mousemove(e) {
+  let axisX = e.clientX - selX;
+  let axisY = e.clientY - selY;
+  selected.style.transform = `translateY(${axisY}px) translateX(${axisX}px)`;
+}
+
+function remove(e) {
+  if (e.target.classList.contains('team-member')) {
+    let draged = selected.innerHTML;
+    selected.innerHTML = e.target.innerHTML;
+    e.target.innerHTML = draged;
+
+    removeDoc();
+  }
+}
+
+function removeDoc() {
+  document.removeEventListener('mousemove', mousemove, true);
+  selected.style.transform = `translateY(0px) translateX(0px)`;
+
+  body.classList.remove('unselectable');
+  selected = '';
+  drag = false;
 }
