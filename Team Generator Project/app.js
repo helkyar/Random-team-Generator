dc5fdbd00b3a0786a7fd8c9164129e3cc1add6d5
helkyar <javierpalaciosbotejara@gmail.com>
2021-10-30 15:45:51 +0200

    Main js functionality

--- Team Generator Project/app.js
+++ Team Generator Project/app.js
@@ -1,28 +1,64 @@
 /**
  * KNOWN ISSUES
- * -addmember animation aplias to all all the time (no way arround delte)
- * -member remove doesn't actualize memberinfo (array doesn't delete)
+ * ->Shuffle on start
+ * ->Interrogants equals nºplayers
+ * ->Generate other distributions (bulk, uniform, Nº Teams)
+ * ->Double click to edit member
+ * 
+ * (~v)->dinamic color team change (good but make it better)
+ * (~v)->Interrogansts move 
+ * 
+ * ->Clear modal: cat that eats the screen
+
  */
-console.log(elements, animals, heroes, villains);
+
+let body = document.querySelector('body');
+const retrieve = document.querySelector('header strong');
+
 let addButton = document.querySelector('.add');
 let teamSize = document.querySelector('#size');
 let nameInput = document.querySelector('#name');
 
 let members = document.querySelector('.inf-memb');
 let infTeams = document.querySelector('.inf-teams');
+const start = document.querySelector('.start');
+const clear = document.querySelector('.clear');
+const save = document.querySelector('.save');
+let saveMsg = document.querySelector('.saved');
 
 let memberList = document.querySelector('.list');
-
 let team = document.querySelector('.teams-wraper');
 
-let teamInitial = team.innerHTML; //starts empty
 let membersNames = [];
+let teamDivs = [];
+let savedTeams;
 
+getLocalStorage();
+// -------------------------------------------
 addButton.addEventListener('click', addMember);
 memberList.addEventListener('click', removeMemb);
-teamSize.addEventListener('input', addTeam);
+teamSize.addEventListener('input', addMockTeam);
 
+start.addEventListener('click', generateTeams);
+clear.addEventListener('click', clearAll);
+save.addEventListener('click', saveToStorage);
+retrieve.addEventListener('click', getLocalStorage);
 // ======================================================
+
+function getLocalStorage() {
+  if (localStorage.getItem('members') != null) {
+    let m = localStorage.getItem('members');
+    let t = localStorage.getItem('teams');
+    let nT = localStorage.getItem('numbTeams');
+    let nM = localStorage.getItem('numbMembers');
+    membersNames = localStorage.getItem('membersNames').split(',');
+
+    memberList.innerHTML = m;
+    t == 'undefined' ? addMockTeam() : (team.innerHTML = t);
+    infoUpdate(nT, nM);
+  }
+}
+
 function addMember(e) {
   e.preventDefault();
 
@@ -36,70 +72,156 @@ function addMember(e) {
     }
 
     nameInput.value = '';
-    addTeam(); // Actualizar al meter los miembros UE
+    addMockTeam(); // Actualizar al meter los miembros UE
   }
 }
 
 function removeMemb(e) {
   let paragraf = e.target.parentElement;
 
-  for (let i = 0; i < membersNames.length; i++) {
-    if (membersNames[i].includes(paragraf.innerText)) {
-      membersNames.splice(i, 1);
-      break;
-    }
-  }
-
   if (e.target.tagName == 'IMG') {
     paragraf.remove();
+    for (let i = 0; i < membersNames.length; i++) {
+      if (membersNames[i].trim() == paragraf.innerText.trim()) {
+        membersNames.splice(i, 1);
+        break;
+      }
+    }
+    addMockTeam(); //calls mock team deleting created teams
   }
-
-  // remove from array, select player
-  infoUpdate(10);
 }
 
-function addTeam() {
+function addMockTeam() {
   let size = 2;
-  let teamDivs = [];
+  teamDivs = [];
+  let extraMemb = membersNames.length % size;
 
   teamSize.value > 2 ? (size = teamSize.value) : '';
+  teamSize.value = size; //show it to the user
+
   let teamNum = Math.ceil(membersNames.length / size);
 
-  infoUpdate(teamNum);
-  // Meter miembros => ???
-  // <p class='team-member'> Juan </p>;
-  for (let i = 0; i < teamNum; i++) {
-    teamDivs.push(`<article class="team"> <h4>Team ${i + 1}</h4>
-    <div class="team-member-list"><p><strong>???</strong></p></div></article><div></div>`);
+  if ((extraMemb != 1 || extraMemb <= teamNum - 1) && extraMemb != 0) {
+    teamNum--;
   }
 
-  team.innerHTML = teamInitial + teamDivs.join('');
+  for (let i = 0; i < teamNum; i++) {
+    teamDivs.push(`<article class="team" style="border: 2px solid 
+    #${112141 + 400 * i}; background-color: #${112141 + 400 * i};"> 
+    <h4 class="tick"><strong>???</strong></h4>
+    <div class="team-member-list">
+    <p class="tick"><strong>???</strong></p>
+    </div></article><div></div>`);
+  }
+  team.innerHTML = teamDivs.join('');
+
+  infoUpdate(teamNum);
+}
+
+function generateTeams() {
+  let teamRand;
+  let teamNames = pickTeamArray();
+  if (teamNames) {
+    teamRand = randomArr(teamDivs.length, teamNames.length);
+  }
+  let random = randomArr(membersNames.length, membersNames.length);
+  let teamNumb = teamDivs.length;
+  team.innerHTML = '';
+
+  for (let a = 0; a < membersNames.length; a++) {
+    if (Math.floor(a / teamNumb) == 0) {
+      team.innerHTML += `<article class="team" 
+      style="border: 2px solid #${112141 + random[a] * (a + 10)}; 
+      background-color: #${112141 + random[a] * (a + 10)};">
+      <h4>${teamNames ? teamNames[teamRand[a]] : 'Team ' + (a + 1)}</h4>
+      <div class="team-member-list">
+      <p class="team-member">${membersNames[random[a]]}</p></div></article>`;
+    } else {
+      let teamMemberList = document.querySelectorAll('.team-member-list');
+      teamMemberList[a % teamNumb].innerHTML += `<p class="team-member"> 
+                                                ${membersNames[random[a]]}</p>`;
+    }
+    savedTeams = team.innerHTML;
+  }
+
+  let teamMember = document.querySelectorAll('.team-member');
+  initializeDrag(teamMember);
+}
+
+function clearAll() {
+  membersNames = [];
+  team.innerHTML = '';
+  memberList.innerHTML = '';
+}
+
+function saveToStorage() {
+  saveMsg.classList.add('show');
+  setTimeout(() => {
+    saveMsg.classList.remove('show');
+  }, 1200);
+
+  let savedMembers = '';
+  for (let member of membersNames) {
+    savedMembers += `<p class="member">
+      <img src="img/trash.svg" alt="delete" />${member}</p>`;
+  }
+
+  localStorage.setItem('teams', savedTeams);
+  localStorage.setItem('members', savedMembers);
+  localStorage.setItem('numbTeams', teamDivs.length); //
+  localStorage.setItem('numbMembers', membersNames.length);
+  localStorage.setItem('membersNames', membersNames);
 }
 // HELPERS --------------------------------------------
 
-function infoUpdate(teamNum) {
-  members.innerText = `Memb.: ${membersNames.length}`;
+function infoUpdate(teamNum, membNum = membersNames.length) {
+  members.innerText = `Memb.: ${membNum}`;
   infTeams.innerHTML = `Teams: ${teamNum}`;
 }
 
-// ======================================================
+function randomArr(arrLength, arrMax) {
+  let random = [];
+  while (random.length < arrLength) {
+    let number = Math.floor(Math.random() * arrMax);
+    if (!random.includes(number)) {
+      random.push(number);
+    }
+  }
+  return random;
+}
 
-let teamMember = document.querySelectorAll('.team-member');
-let body = document.querySelector('body');
+function pickTeamArray() {
+  let namesArr = document.querySelector('#teams-name');
+  let finalArr = [];
+  if (namesArr.value == 'element') {
+    finalArr = elements;
+  } else if (namesArr.value == 'animal') {
+    finalArr = animals;
+  } else if (namesArr.value == 'heroes') {
+    finalArr = heroes;
+  } else if (namesArr.value == 'villains') {
+    finalArr = villains;
+  } else {
+    finalArr = null;
+  }
+  return finalArr;
+}
+
+// DRAG & DROP ======================================================
 let drag = false;
 let selected;
 let selY;
 let selX;
 
-for (let i = 0; i < teamMember.length; i++) {
-  teamMember[i].addEventListener('mousedown', move);
-  teamMember[i].addEventListener('mouseup', remove);
+function initializeDrag(teamMember) {
+  for (let i = 0; i < teamMember.length; i++) {
+    teamMember[i].addEventListener('mousedown', move);
+    teamMember[i].addEventListener('mouseup', remove);
+  }
 }
 
 document.addEventListener('mouseup', removeDoc);
-document.onmusedown = () => {
-  return false;
-};
+
 function move(e) {
   if (e.target.classList.contains('team-member')) {
     drag = true;
@@ -111,13 +233,12 @@ function move(e) {
   }
 }
 
-function removeSelection() {
-  return false;
-}
 function mousemove(e) {
   let axisX = e.clientX - selX;
   let axisY = e.clientY - selY;
   selected.style.transform = `translateY(${axisY}px) translateX(${axisX}px)`;
+  selected.style.border = 'solid 0.5px #000';
+  selected.style.position = 'sticky';
 }
 
 function remove(e) {
@@ -135,7 +256,9 @@ function removeDoc() {
 
   //avoid console error
   if (selected) {
-    selected.style.transform = `translateY(0px) translateX(0px)`;
+    selected.style.transform = `translateY(0px) translateX(-0.5px)`;
+    selected.style.border = 'none';
+    selected.style.position = 'inherit';
     selected = '';
   }
 
